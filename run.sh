5728dd11cdd0cb001a873d00d522655f376040b6
Jianyong Wu <616191051@qq.com>
2020-03-28 10:47:58 +0000

    change memory size to 1G

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 arch=$(uname  -m)
 if [ "$arch" = "aarch64" ]; then
-#	/root/qemu/aarch64-softmmu/qemu-system-aarch64 -M virt -cpu host --enable-kvm -m 1024 -kernel armos -nographic -serial mon:stdio -s -S
 	echo "it's aarch64"
-	/root/qemu/qemu/aarch64-softmmu/qemu-system-aarch64 -M virt -cpu host --enable-kvm -m 4G -kernel armos -nographic -serial mon:stdio  -s
+	qemu-system-aarch64 -M virt -cpu host --enable-kvm -m 1G -kernel armos -nographic -serial mon:stdio  -s
 else
 	qemu-system-aarch64 -M virt -cpu cortex-a57 -m 2G -kernel armos -nographic -serial mon:stdio 
 fi
