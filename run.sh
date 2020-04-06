f85203da4f8ce4b284be9e402a5a56ea01fa871b
Jianyong Wu <616191051@qq.com>
2020-04-06 13:02:42 +0000

    run: add gicv2 to virt machine in startup script.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 arch=$(uname  -m)
 if [ "$arch" = "aarch64" ]; then
 	echo "it's aarch64"
-	qemu-system-aarch64 -M virt -cpu host --enable-kvm -m 1G -kernel armos -nographic -serial mon:stdio  -s
+	qemu-system-aarch64 -M virt,gic-version=2 -cpu host --enable-kvm -m 1G -kernel armos -nographic -serial mon:stdio  -s 
 else
 	qemu-system-aarch64 -M virt -cpu cortex-a57 -m 2G -kernel armos -nographic -serial mon:stdio 
 fi
